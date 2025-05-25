727593f4bb8a08f938d8d2169d2d0746d952f8a1
Oscar Ortega <116125386+microtec9@users.noreply.github.com>
2025-05-25 19:55:11 -0400

    Archivo Prueba
    Para tarea

---
+++ 9.sql
@@ -0,0 +1,12 @@
+INSERT INTO reserva (id_cliente, id_hotel, fecha_reserva, num_noches, estado)
+VALUES 
+(1, 26, '2025-02-16', 3, 'Confirmada'),
+(2, 27, '2025-02-17', 5, 'Pendiente'),
+(3, 28, '2025-02-18', 2, 'Confirmada'),
+(4, 29, '2025-02-19', 4, 'Cancelada'),
+(5, 30, '2025-02-20', 6, 'Confirmada'),
+(6, 31, '2025-02-21', 3, 'Pendiente'),
+(7, 32, '2025-02-22', 2, 'Confirmada'),
+(8, 33, '2025-02-23', 7, 'Cancelada'),
+(9, 34, '2025-02-24', 1, 'Pendiente'),
+(10, 35, '2025-02-25', 4, 'Confirmada');
